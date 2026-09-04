9c73572e680ea9e012f595aefea0b0a9767690dc
5andsun <song302ms@naver.com>
2026-09-04 12:11:45 +0000

    더미 데이터 작성

---
+++ scripts/dummyDatas.js
@@ -0,0 +1,64 @@
+const movies = [
+    {
+        id: 1,
+        title: "타짜",
+        info: {
+            poster: "포스터이미지링크",
+            director: "감독이름",
+            genre: "장르",
+            releaseYear: "개봉년도",
+            actors: ["배우1", "배우2", "배우3"]
+        },
+        watched: "날짜",
+        rating: 4.5
+    },
+    {
+        id: 2,
+        title: "도둑들",
+        info: {
+            poster: "포스터이미지링크",
+            director: "감독이름",
+            genre: "장르",
+            releaseYear: "개봉년도",
+            actors: ["배우1", "배우2", "배우3"]
+        },
+        watched: "날짜",
+        rating: 4.5
+    },
+    {
+        id: 3,
+        title: "괴물",
+        info: {
+            poster: "포스터이미지링크",
+            director: "감독이름",
+            genre: "장르",
+            releaseYear: "개봉년도",
+            actors: ["배우1", "배우2", "배우3"]
+        },
+        watched: "날짜",
+        rating: 4.5
+    }
+]
+
+const users = [
+    {
+        id: 1,
+        username: "밥",
+        password: "1279"
+    },
+    {
+        id: 2,
+        username: "샤",
+        password: "0908"
+    },
+    {
+        id: 3,
+        username: "산",
+        password: "1111"
+    },
+    {
+        id: 4,
+        username: "달",
+        password: "1234"
+    }
+]
